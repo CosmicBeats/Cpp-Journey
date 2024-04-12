8ab808a7b2bd08daaf00023c449d905d7b1bb5da
Paulo Castro <cosmicwhistle@gmail.com>
2024-04-12 00:58:59 -0400

    Added a function that prints text to the console slowly

--- ProjectTerminal.cpp
+++ ProjectTerminal.cpp
@@ -3,7 +3,18 @@
 #include <vector>
 #include <string>
 
+void PrintSlowText(std::string text);
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    PrintSlowText("Yoooooooo!!!!\n");
+}
+
+void PrintSlowText(std::string text)
+{
+    for (int i = 0; i < text.length(); i++)
+    {
+        std::cout << text[i];
+        Sleep(25);
+    }
 }
