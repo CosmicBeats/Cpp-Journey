0e8473363735ef6e538d27fe4c8144541759dd10
Paulo Castro <cosmicwhistle@gmail.com>
2024-04-12 01:12:59 -0400

    Added 10 topics for the user to choose from

--- ProjectTerminal.cpp
+++ ProjectTerminal.cpp
@@ -7,7 +7,16 @@ void PrintSlowText(std::string text);
 
 int main()
 {
-    PrintSlowText("Yoooooooo!!!!\n");
+    // Needed variables to work with ^.^
+    std::vector<std::string> topics {"1.  C++ Basics", "2.  C++ Variables and Constants", "3.  C++ Data Types and Literals", "4.  C++ Operators", "5.  C++ Input/Output",
+                              "6.  C++ Control Statements", "7.  C++ Funtions", "8.  C++ Pointer and References", "9.  C++ Arrays", "10. C++ Strings"};
+
+    PrintSlowText("Hello user!!!\nWhat would you like to review today?\n\n");
+
+    for (int i = 0; i < topics.size(); i++)
+    {
+        std::cout << topics[i] << std::endl;
+    }
 }
 
 void PrintSlowText(std::string text)
