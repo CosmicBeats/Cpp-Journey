5108e6f9940c995cc4c8708c1132bb00772686c0
Paulo Castro <cosmicwhistle@gmail.com>
2024-04-13 09:58:30 -0400

    Refactored the data structure and added more libraries to work with

--- ProjectTerminal.cpp
+++ ProjectTerminal.cpp
@@ -1,38 +1,68 @@
 #include <iostream>
 #include <Windows.h>
 #include <vector>
-#include <unordered_map>
 #include <string>
+#include <algorithm>
+#include <cctype>
 
 void Start();
 void PrintSlowText(std::string text);
 void UserValidation(int& pUserInput);
-void DisplayTopic(int& pUserInput);
+void DisplaySubTopic(int& pUserInput);
 
 int main()
 {
-    void Start();
+    Start();
 }
 
 void Start()
 {
     int userInput = 0;
 
-    std::vector<std::string> topics{ "1.  C++ Basics", "2.  C++ Variables and Constants", "3.  C++ Data Types and Literals", "4.  C++ Operators", "5.  C++ Input/Output",
-                              "6.  C++ Control Statements", "7.  C++ Funtions", "8.  C++ Pointer and References", "9.  C++ Arrays", "10. C++ Strings" };
+    std::vector<std::pair<std::string, std::vector<std::string>>> topics = 
+    {
+        {"C++ Programming Language",    {"What is C++?", "Why Learn C++?"}},
+
+        {"C++ Basics",                  {"First C++ Program", "C++ Basic Syntax", "C++ Comments", "C++ Tokens", "C++ Identifiers", "C++ Keywords", "Difference between Keyword and Identifier"}},
+
+        {"C++ Variables and Constants", {"C++ Variables", "C++ Constants", "Scope of C++ Variables", "C++ Storage Classes", "C++ Static Variables"}},
+
+        {"C++ Data Types and Literals", {"C++ Data Types", "C++ Literals", "C++ Derived Data Types", "C++ User-Defined Data Types", "C++ Data Type Ranges and Their Macros", 
+                                         "C++ Type Modifiers", "C++ Data Type Conversion", "C++ Typecasting Operators"}},
+
+        {"C++ Operators",               {"C++ Operators", "C++ Arithmetic Operators", "C++ Unary Operators", "C++ Bitwise Operators", "C++ Relational Operators", "C++ Logical Operators",
+                                         "C++ Assignment Operators", "C++ Ternary/Conditional Operators", "C++ Sizeof Operator", "C++ Scope Resolution Operator"}},
+
+        {"C++ Input/Output",            {"C++ Basic Input / Output", "C++ Standard Input Stream (cin)", "C++ Standard Output Stream (cout)", "C++ Standard Error Stream (cerr)", 
+                                         "C++ Input / Output Manipulator"}},
+
+        {"C++ Control Statements",      {"C++ Decision Making", "C++ if Statement", "C++ if-else Statement", "C++ if-else-if Ladder", "C++ Nested if-else Statement", "C++ Switch Statement",
+                                         "C++ Jump Statements", "C++ Loops", "C++ for Loop", "C++ Range-Based for Loop", "C++ while Loop", "C++ do…while Loop"}},
+
+        {"C++ Functions",               {"C++ Functions", "C++ return", "C++ Parameter Passing Techniques", "Difference between Call by Value and Call by Reference", "C++ Default Arguments",
+                                         "C++ Recursion", "C++ Inline Functions", "C++ Lambda Expression"}},
+
+        {"C++ Pointers and References", {"C++ Pointers and References", "C++ Pointers", "C++ Pointer Arithmetic", "Dangling, Void, Null, and Wild Pointers", "Applications of Pointers",
+                                         "C++ nullptr", "C++ References", "Can references refer to an invalid location in C++?", "Difference Between Pointers and References in C++",
+                                         "Passing by pointer Vs Passing by Reference in C++", "When do we pass arguments by reference or pointer?"}},
+
+        {"C++ Arrays",                  {"C++ Arrays", "C++ Multidimensional Arrays", "C++ Pointer to an Array", "Size of Array parameter", "Passing Arrays to Functions in C++",
+                                         "What is Array Decay in C++? How can it be prevented?"}}
+    };
+                                                                            
 
     PrintSlowText("Hello user!!!\nWhat would you like to review today?\n\n");
 
-    for (int i = 0; i < topics.size(); i++)
+    for (const auto& mainTopic : topics)
     {
-        std::cout << topics[i] << std::endl;
+        std::cout << mainTopic.first << std::endl;
     }
 
     PrintSlowText("\n\nPlease choose a number:");
     UserValidation(userInput);
 }
 
-void DisplayTopic(int& pUserInput)
+void DisplaySubTopic(int& pUserInput)
 {
     switch(pUserInput)
     {
